c982d4105923c2d061d2bb1594cb627f298faa29
amirinsamsudin <amirinsamsudin@gmail.com>
2019-10-09 09:32:35 +0800

    Native Device Features

--- src/app/pages/film-details/film-details.page.ts
+++ src/app/pages/film-details/film-details.page.ts
@@ -1,6 +1,7 @@
 import { ApiService } from './../../services/api.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
 @Component({
   selector: 'app-film-details',
@@ -11,13 +12,24 @@ export class FilmDetailsPage implements OnInit {
 
   film: any;
 
-  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) { }
+  constructor(private activatedRoute: ActivatedRoute, private api: ApiService, private emailComposer: EmailComposer) { }
 
   ngOnInit() {
     let id = this.activatedRoute.snapshot.paramMap.get('id');
     this.api.getFilm(id).subscribe(res => {
       this.film = res;
     });
+  }
+
+  shareFilm() {
+    let email = {
+      to: 'khairul@eass.com.my',
+      subject: 'I love this one: ' + this.film.title,
+      body: 'Can you remember the opening?<br><br>\"' + this.film.opening_crawl + '\"',
+      isHtml: true
+    };
+ 
+    this.emailComposer.open(email);
   }
 
 }
