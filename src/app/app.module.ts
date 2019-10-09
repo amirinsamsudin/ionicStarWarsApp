8970aaeb7d079df7d0db995a06d84f33ad6725bf
amirinsamsudin <amirinsamsudin@gmail.com>
2019-10-09 09:56:26 +0800

    Storing Data inside Ionic Apps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
+import { IonicStorageModule } from '@ionic/storage';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
-    HttpClientModule],
+    HttpClientModule, IonicStorageModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/pages/film-details/film-details.page.ts
+++ src/app/pages/film-details/film-details.page.ts
@@ -1,3 +1,4 @@
+import { FavoriteService } from './../../services/favorite.service';
 import { ApiService } from './../../services/api.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -11,24 +12,44 @@ import { EmailComposer } from '@ionic-native/email-composer/ngx';
 export class FilmDetailsPage implements OnInit {
 
   film: any;
+  isFavorite = false;
+  filmId = null;
 
-  constructor(private activatedRoute: ActivatedRoute, private api: ApiService, private emailComposer: EmailComposer) { }
+  constructor(private activatedRoute: ActivatedRoute, private api: ApiService, private emailComposer: EmailComposer, private favoriteService: FavoriteService) { }
 
   ngOnInit() {
-    let id = this.activatedRoute.snapshot.paramMap.get('id');
-    this.api.getFilm(id).subscribe(res => {
+    this.filmId = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.api.getFilm(this.filmId).subscribe(res => {
       this.film = res;
+    });
+
+    this.favoriteService.isFavorite(this.filmId).then(isFav => {
+      this.isFavorite = isFav;
     });
   }
 
+  favoriteFilm() {
+    this.favoriteService.favoriteFilm(this.filmId).then(() => {
+      this.isFavorite = true;
+    });
+  }
+ 
+  unfavoriteFilm() {
+    this.favoriteService.unfavoriteFilm(this.filmId).then(() => {
+      this.isFavorite = false;
+    });
+  }
+
+
   shareFilm() {
     let email = {
-      to: 'khairul@eass.com.my',
+      to: 'saimon@devdactic.com',
       subject: 'I love this one: ' + this.film.title,
       body: 'Can you remember the opening?<br><br>\"' + this.film.opening_crawl + '\"',
       isHtml: true
     };
- 
+
     this.emailComposer.open(email);
   }
 
